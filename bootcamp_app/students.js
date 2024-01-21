ed55a8fd717023aa3e18a9f446b175c0d24711a4
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-01-21 11:04:44 -0500

    paramterized the queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,7 +7,8 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 const cohortname = process.argv[2]
-const numOfResults = process.argv[3]
+const numOfResults = process.argv[3] || 5;
+const values = [cohortname, numOfResults];
 
 // pool.query(`
 // SELECT id, name, cohort_id
@@ -38,9 +39,9 @@ pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE '%$1%'
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
